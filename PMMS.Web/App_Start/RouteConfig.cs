a41384b0eb1809419e4977445d25153a90b5ef4e
alls4f3 <165911704+alls4f3@users.noreply.github.com>
2024-12-26 12:54:32 +0200

    source

---
+++ PMMS.Web/App_Start/RouteConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PMMS
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("Content/{resource}.axd/{*pathInfo}");
+         
+            routes.MapMvcAttributeRoutes();
+
+
+        }
+
+
+        public static class WebApiConfig
+        {
+            public static void Register(HttpConfiguration config)
+            {
+                // Web API configuration and services
+
+                // Web API routes
+                config.MapHttpAttributeRoutes();
+
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            }
+        }
+    }
+}
---
+++ PMMS.Web/App_Start/SiteInitialization.cs
@@ -0,0 +1,66 @@
+﻿namespace PMMS
+{
+    using Administration;
+    using Serenity;
+    using Serenity.Abstractions;
+    using Serenity.Data;
+    using Serenity.Web;
+    using System;
+    using System.Configuration;
+
+    public static partial class SiteInitialization
+    {
+        public static void ApplicationStart()
+        {
+            try
+            {
+                SqlSettings.AutoQuotedIdentifiers = true;
+                Serenity.Web.CommonInitialization.Run();
+
+                var registrar = Dependency.Resolve<IDependencyRegistrar>();
+                registrar.RegisterInstance<IAuthorizationService>(new Administration.AuthorizationService());
+                registrar.RegisterInstance<IAuthenticationService>(new Administration.AuthenticationService());
+                registrar.RegisterInstance<IPermissionService>(new LogicOperatorPermissionService(new Administration.PermissionService()));
+                registrar.RegisterInstance<IUserRetrieveService>(new Administration.UserRetrieveService());
+               
+
+                if (!ConfigurationManager.AppSettings["LDAP"].IsTrimmedEmpty())
+                    registrar.RegisterInstance<IDirectoryService>(new LdapDirectoryService());
+
+                if (!ConfigurationManager.AppSettings["ActiveDirectory"].IsTrimmedEmpty())
+                    registrar.RegisterInstance<IDirectoryService>(new ActiveDirectoryService());
+
+                InitializeExceptionLog();
+
+                if (ConfigurationManager.AppSettings.Get("EnableOffline") == "1")
+                {
+               PavePixel.SiteInitializationSetup.ApplicationStartOffline();
+
+                }
+                else
+                {
+                    PavePixel.SiteInitializationSetup.ApplicationStart();
+
+                }
+
+
+
+            }
+            catch (Exception ex)
+            {
+                ex.Log();
+                throw;
+            }
+
+            foreach (var databaseKey in databaseKeys)
+            {
+                //EnsureDatabase(databaseKey);
+                //RunMigrations(databaseKey);
+            }
+        }
+
+        public static void ApplicationEnd()
+        {
+        }
+    }
+}
